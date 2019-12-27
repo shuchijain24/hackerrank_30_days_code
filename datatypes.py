bbaa26ca450ce216f8854908f24df4dc5e5ba1a9
shuchijain24 <shuchi.24jain@gmail.com>
2019-12-27 21:43:00 +0530

    Day_1_code

---
+++ datatypes.py
@@ -0,0 +1,20 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+ii = 12
+dd = 4.0
+ss = 'is the best place to learn and practice coding!'
+
+# Read and save an integer, double, and String to your variables.
+ii= int(input())
+dd= float(input())
+ss= str(input())
+# Print the sum of both integer variables on a new line.
+print(i+ii)
+print(d+dd)
+print(s+ ss)
+
+
+
