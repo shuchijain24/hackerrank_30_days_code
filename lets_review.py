966d8abadd7d63686ba9d639082b46d11c086c82
shuchijain24 <shuchi.24jain@gmail.com>
2019-12-29 17:42:43 +0530

    Day_6_code

---
+++ lets_review.py
@@ -0,0 +1,14 @@
+num_test = int(input())
+
+for i in range (num_test):
+    test_string = input()
+    even_indexed_characters = ''
+    odd_indexed_characters = ''
+
+    for j in range (len(test_string)):
+        if j % 2 == 0:
+            even_indexed_characters += test_string[j]
+        else:
+                odd_indexed_characters += test_string[j]
+
+    print('{} {}'. format(even_indexed_characters,odd_indexed_characters))
